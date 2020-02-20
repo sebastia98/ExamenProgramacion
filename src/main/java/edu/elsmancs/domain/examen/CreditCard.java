f70b2e3064e737c563cf9ebce147241e47177d23
sebastia98 <sadrover@cifpfbmoll.eu>
2020-02-20 17:29:15 +0100

    Feat: Crear constructor TarjetaCredito

---
+++ src/main/java/edu/elsmancs/domain/examen/CreditCard.java
@@ -0,0 +1,15 @@
+package edu.elsmancs.domain.examen;
+
+public class CreditCard {
+	
+	private String nombre = null;
+	private String numeroTarjeta = null;
+
+	public CreditCard(String nombreInvitado, String tarjetaCredito) {
+		this.nombre = nombreInvitado;
+		this.numeroTarjeta = tarjetaCredito;
+	}
+	
+	
+
+}
---
+++ src/test/java/edu/elsmancs/domain/examen/CreditCardTest.java
@@ -0,0 +1,14 @@
+package edu.elsmancs.domain.examen;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CreditCardTest {
+
+	@Test
+	public void test() {
+		
+	}
+
+}
